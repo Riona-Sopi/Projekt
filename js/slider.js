ff68a27645ec62c8d9f32ddacb0154879d54b8a8
Riona-Sopi <75809684+Riona-Sopi@users.noreply.github.com>
2020-12-22 22:50:47 +0100

    New Responsive Slider

--- js/slider.js
+++ js/slider.js
@@ -1,41 +1,28 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
+var slideIndex = 1;
+showSlides(slideIndex);
 
-//Buttons
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-//Counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-//Button Listeners
-nextBtn.addEventListener('click', () =>{
-    if (counter >= carouselImages.length - 1) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter++;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-prevBtn.addEventListener('click', () =>{
-    if (counter <= 0) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter--;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-carouselSlide.addEventListener('transitionend', () =>{
-    if(carouselImages[counter].id === 'lastpicClone'){
-        carouselSlide.style.transition ="none";
-        counter = carouselImages.length - 2;
-        carouselSlide.style.transform ='translateX(' + (-size * counter) + 'px)';
-        
-    }
-    if(carouselImages[counter].id === 'firstpicClone'){
-        carouselSlide.style.transition ="none";
-        counter = carouselImages.length - counter;
-        carouselSlide.style.transform ='translateX(' + (-size * counter) + 'px)';
-        
-    }
-});
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
