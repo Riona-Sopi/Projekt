76666894dcb37e76239df486089450b7ade16c87
Qendrim Cakaj <qcakaj@gmail.com>
2021-01-13 20:47:01 +0100

    Added contactform class with functions to query db

---
+++ classes/contactform.class.php
@@ -0,0 +1,34 @@
+<?php 
+
+class ContactForm extends Dbh {
+  public function getAllContactForms() {
+    $sql = "SELECT * FROM contact_form
+    ORDER BY id DESC;";
+    $stmt = $this->connect()->prepare($sql);
+    $stmt->execute();
+
+    while($result = $stmt->fetchAll()) {
+      return $result;
+    };
+  }
+
+  public function getContactFormByEmail($email){
+      $sql = "SELECT * FROM contact_form WHERE email = ?";
+      $stmt = $this->connect()->prepare($sql);
+      $stmt-> execute([$email]);
+      $result = $stmt -> fetch();
+      return $result;
+  }
+
+  public function addContactForm($name,$mAddress,$email,$phoneNumber,$mMessage) {
+    $sql = "INSERT INTO contact_form(fullname, address, email, phoneNumber, message) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $this->connect()->prepare($sql);
+    $stmt->execute([$name, $mAddress,$email,$phoneNumber,$mMessage]);
+  }
+
+  public function deleteContactForm($id) {
+    $sql = "DELETE FROM contact_form WHERE id = ?";
+    $stmt = $this->connect()->prepare($sql);
+    $stmt->execute([$id]);
+  }
+}
