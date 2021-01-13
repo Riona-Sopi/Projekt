38a7c3495882b7dba41cb429657f708be4ec42ce
Qendrim Cakaj <qcakaj@gmail.com>
2021-01-13 20:48:04 +0100

    added footer as an include

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col-1">
+                <img src="images/logo.png" alt="">
+            </div>
+            <div class="footer-col-2">
+                <h3>Useful Links</h3>
+                <ul>
+                    <li>Privacy Policy </li>
+                    <li>Terms and Contitions</li>
+                    <li> Coupons</li>
+                </ul>
+            </div>
+            <div class="footer-col-3">
+                <h3>Follow us</h3>
+                <ul>
+                    <li>Twitter</li>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                </ul>
+            </div>
+
+        </div>
+        <hr>
+        <div class="copyright">Copyright 2020 - Hustlers</div>
+    </div>
+</div>
